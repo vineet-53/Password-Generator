eeacb1d9af5fe2ba52455cfc89a5163a6ddb9595
vineet-53 <vineetsinghnegi53@gmail.com>
2023-08-24 12:19:09 +0530

    feat: strong or weak password prompt

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-const passwordStrenghtElem = document.querySelector(
+const passwordStrengthElem = document.querySelector(
   "span[data-password-strength]"
 );
 const passwordLengthElem = document.querySelector(
@@ -10,28 +10,55 @@ const passwordLengthText = document.querySelector(
 const generatePassword = document.querySelector(
   "button[data-password-generate]"
 );
+const checkBoxNodeList = document.querySelectorAll("input[type=checkbox]");
+const checkBoxList = Array.from(checkBoxNodeList);
+const passwordStrengthTextElem = document.querySelector('span[data-password-strength-text]')
 const passwordCopyElem = document.querySelector("span[data-password-copy]");
 let passwordLength = 0;
 
-// update ui after any changes 
-function updateUi() {
+// update ui after any changes
+function updateUi(checkedBoxesList = []) {
   passwordLengthText.textContent = passwordLength;
+  if (checkedBoxesList.length > 2 && passwordLength > 10) {
+    // update the color of strength to green
+    passwordStrengthElem.classList.add("strong");
+    passwordStrengthElem.classList.remove("weak");
+    passwordStrengthTextElem.textContent = "Strong"
+  } else {
+    // update the color or strength red
+    passwordStrengthElem.classList.remove("strong");
+    passwordStrengthElem.classList.add("weak");
+    passwordStrengthTextElem.textContent = "Weak"
+  }
 }
 // setting password if length is changing
-function setPasswordLength() {
+function setPasswordLength(event) {
   passwordLength = Math.floor(event.target.value / 4);
 }
-
+// checking the password strenght
+function checkPasswordStrength() {
+  let checkedBoxesList = checkBoxList.filter((input) => {
+    if (input.checked) return input;
+  });
+  updateUi(checkedBoxesList);
+}
 // handle generate
+
 function handleGeneratePassword() {
   console.log("password is being generated");
 }
-passwordLengthElem.addEventListener("change", (event) => {
-  setPasswordLength();
+checkBoxList.forEach((checkBox) => {
+  checkBox.addEventListener("click", () => {
+    checkPasswordStrength();
+  });
+});
+passwordLengthElem.addEventListener("click", (event) => {
+  setPasswordLength(event);
   updateUi();
+  checkPasswordStrength();
 });
 
-
 generatePassword.addEventListener("click", (e) => {
+  checkPasswordStrength();
   handleGeneratePassword();
 });
